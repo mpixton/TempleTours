7585c253e6098ab6b38c63dc945632ea6932970a
mpixton <71086131+mpixton@users.noreply.github.com>
2021-03-13 09:24:06 -0700

    FEAT: add models to project
    Added the Tour and TourParty models to the project. Tours represent a specific instance of a Tour of the Temple. A TourParty represents the group of people signing up for a specific time slot (Tour).

---
+++ Models/Tour.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+// TODO: talk with team about adding a max number of people in appt.
+namespace TempleTours.Models
+{
+    /// <summary>
+    /// Model for a Tour instance of the temple. Contains the Date and Time of the Tour 
+    /// as well as the list of Tourists for the Tour.
+    /// </summary>
+    public class Tour
+    {
+        /// <summary>
+        /// Read only database field. PK of the Tour.
+        /// </summary>
+        [Key]
+        public int TourId { get; }
+
+        /// <summary>
+        /// Date and Time of the Tour.
+        /// </summary>
+        [Required]
+        public DateTime TourTime { get; set; }
+
+        /// <summary>
+        /// List of Tourists attending the Tour. Creates a one to many relationship between the entities. 
+        /// TourParty will have a FK field with the PK of the Tour instance they are attending.
+        /// </summary>
+        public List<TourParty> Parties { get; set; }
+
+        // TODO: add a property that returns the size of all parties for this slot.
+
+        /// <summary>
+        /// Adds a party to the Tour Time.
+        /// </summary>
+        /// <param name="party">TourParty instance to add to the list of attending Tour Parties.</param>
+        public void AddTourist (TourParty party)
+        {
+            Parties.Add(party);
+        }
+    }
+}
