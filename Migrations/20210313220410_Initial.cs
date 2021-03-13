1e9ba9a46246ae504235fe47ae6a417c3d24d034
mpixton <71086131+mpixton@users.noreply.github.com>
2021-03-13 15:15:36 -0700

    DATA: add migrations
    Created the initial migrations. Should be the only migrations we need. Run "dotnet ef database update" to get on the same page.

---
+++ Migrations/20210313220410_Initial.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TempleTours.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tours",
+                columns: table => new
+                {
+                    TourId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TourTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tours", x => x.TourId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TourParty",
+                columns: table => new
+                {
+                    TourPartyId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    PartySize = table.Column<int>(type: "INTEGER", nullable: false),
+                    TourId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TourParty", x => x.TourPartyId);
+                    table.ForeignKey(
+                        name: "FK_TourParty_Tours_TourId",
+                        column: x => x.TourId,
+                        principalTable: "Tours",
+                        principalColumn: "TourId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TourParty_TourId",
+                table: "TourParty",
+                column: "TourId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TourParty");
+
+            migrationBuilder.DropTable(
+                name: "Tours");
+        }
+    }
+}
