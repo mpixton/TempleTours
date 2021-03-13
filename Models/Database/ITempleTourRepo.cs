2a964d1fc3459aea048f8648f7dc99c6e0127337
mpixton <71086131+mpixton@users.noreply.github.com>
2021-03-13 15:18:40 -0700

    DATA: add database files
    Added the necessary files for the DB to talk to the app. Went with the Repo approach as opposed to the raw DbContext approach mentioned in the videos. Repo only has a Tours queryset currently.
    
    Added a SeedData class to get us all on the same page. SeedData will create Tours occuring on the hour, from 8 AM to 8PM, everyday from 13 Mar 2021 to 27 Mar 2021.

---
+++ Models/Database/ITempleTourRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempleTours.Models.Database
+{
+    public interface ITempleTourRepo
+    {
+        IQueryable<Tour> Tours { get; }
+    }
+}
