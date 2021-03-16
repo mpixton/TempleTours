b4020ce930f4ef32b8a8b76694252bee05af8a6f
mpixton <71086131+mpixton@users.noreply.github.com>
2021-03-16 17:12:02 -0600

    FEAT: add viewmodel for userform
    Created a ViewModel to the UserForm signup so that the UserForm has access to the Tour instance as well as TourParty properties.

---
+++ Models/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempleTours.Models.ViewModels
+{
+    public class SignUpViewModel
+    {
+        /// <summary>
+        /// Tour object that the party is trying to sign up for.
+        /// </summary>
+        public Tour Tour { get; set; }
+
+        /// <summary>
+        /// Party's details to input to sign up for the Tour instance.
+        /// </summary>
+        public TourParty Party { get; set; }
+    }
+}
