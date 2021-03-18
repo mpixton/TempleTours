274543b35511e69bd7564a508518550db2aa2994
mpixton <71086131+mpixton@users.noreply.github.com>
2021-03-18 10:42:04 -0600

    REF: rework all models and view models
    Renamed TourParty to just Party. Added navigation properties to Party on Tour. Removed a useless method from Tour (AddParty). The SignUpViewModel now contains all data for a Party and the TourId, but not the Tour. Changing the UserForm view to use a Party was throwing a required error due to Tour not being set.

--- Models/ViewModels/SignUpViewModel.cs
+++ Models/ViewModels/SignUpViewModel.cs
@@ -1,20 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TempleTours.Models.ViewModels
 {
+    /// <summary>
+    /// Allows us to get information from the user without breaking validation.
+    /// </summary>
     public class SignUpViewModel
     {
         /// <summary>
-        /// Tour object that the party is trying to sign up for.
+        /// Foreign Key of the Tour object associated with the Party. 
+        /// Explicitly sets the FK relationship to Tour.
         /// </summary>
-        public Tour Tour { get; set; }
+        [Required]
+        public int TourId { get; set; }
 
         /// <summary>
-        /// Party's details to input to sign up for the Tour instance.
+        /// Name of the party.
         /// </summary>
-        public TourParty Party { get; set; }
+        [Required]
+        public string PartyName { get; set; }
+
+        /// <summary>
+        /// Total number of people in the Party.
+        /// </summary>
+        [Required]
+        [Display(Description = "How many people are in your party (including yourself)?")]
+        public int PartySize { get; set; }
+
+        /// <summary>
+        /// Email address to reach the party leader at.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Optional. Phone number to reach the party leader.
+        /// </summary>
+        [Phone]
+        public string PhoneNumber { get; set; }
     }
 }
